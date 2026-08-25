91c3d76d8cc12983d0a711913b391cb213048fad
Nick Salerni <nick.salerni@gmail.com>
2026-08-25 16:10:26 -0700

    Release mojo-http2 0.2.5 (#31)

--- packaging/test_installed_package.py
+++ packaging/test_installed_package.py
@@ -141,7 +141,7 @@ def main() -> None:
         run(build_command, PACKAGE_BUILD_TIMEOUT_SECONDS)
 
         package = one_package(
-            output, "mojo-http2-0.2.4-*.conda", "mojo-http2 0.2.4"
+            output, "mojo-http2-0.2.5-*.conda", "mojo-http2 0.2.5"
         )
 
         test_command = [
